0ad2a051fab745dadef282290eb98ee8e9f202d8
César N <ceesar90@gmail.com>
2019-07-04 22:52:38 -0300

    Adicionar app

---
+++ live/precache-manifest.113c6a7ab5cd5aef7e713fe0a86b050a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9712bcc2082deefe671dc8b8ec2a968c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cb78f96976e569ecb35",
+    "url": "/static/css/main.4299c822.chunk.css"
+  },
+  {
+    "revision": "fff81176d5d12ecf829b",
+    "url": "/static/js/2.3019fad4.chunk.js"
+  },
+  {
+    "revision": "4cb78f96976e569ecb35",
+    "url": "/static/js/main.5cc4ad07.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d92216467c24205442f0057f885b5da",
+    "url": "/static/media/Monaco.5d922164.woff"
+  },
+  {
+    "revision": "9c8ce100f746e85326f990759a94c79a",
+    "url": "/static/media/Raleway-ExtraLightItalic.9c8ce100.woff"
+  },
+  {
+    "revision": "e5790b9d2d87e17bf055d8ab32d16654",
+    "url": "/static/media/Raleway-Light.e5790b9d.woff"
+  },
+  {
+    "revision": "709a4a4aae36d73b234295aabca0c8f1",
+    "url": "/static/media/Raleway-Regular.709a4a4a.woff"
+  }
+]);
