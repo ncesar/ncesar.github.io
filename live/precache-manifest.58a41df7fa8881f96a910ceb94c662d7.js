20992bde508bf9175c711eb9be7efb06f9bf5168
César N <ceesar90@gmail.com>
2019-07-04 23:24:22 -0300

    App

---
+++ live/precache-manifest.58a41df7fa8881f96a910ceb94c662d7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03feb7338ff384a845009ab903b180c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37711394a1eba637d1ba",
+    "url": "/static/css/main.369f4735.chunk.css"
+  },
+  {
+    "revision": "fff81176d5d12ecf829b",
+    "url": "/static/js/2.3019fad4.chunk.js"
+  },
+  {
+    "revision": "37711394a1eba637d1ba",
+    "url": "/static/js/main.5cc4ad07.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d92216467c24205442f0057f885b5da",
+    "url": "/static/media/Monaco.5d922164.woff"
+  },
+  {
+    "revision": "9c8ce100f746e85326f990759a94c79a",
+    "url": "/static/media/Raleway-ExtraLightItalic.9c8ce100.woff"
+  },
+  {
+    "revision": "e5790b9d2d87e17bf055d8ab32d16654",
+    "url": "/static/media/Raleway-Light.e5790b9d.woff"
+  },
+  {
+    "revision": "709a4a4aae36d73b234295aabca0c8f1",
+    "url": "/static/media/Raleway-Regular.709a4a4a.woff"
+  }
+]);
